1fc697fea92241b9ca3dd48a1c15e2a988632cae
Fabien LOISON <flo@flogisoft.com>
2019-08-28 10:43:31 +0200

    Fixes vaulttec111 mouse name

--- rivalcfg/profiles/rival300vaulttec111.py
+++ rivalcfg/profiles/rival300vaulttec111.py
@@ -1,7 +1,7 @@
 from .rival import rival
 
 rival300vaulttec111 = {
-    "name": "SteelSeries Rival 300 Fallout 4 Vault Tec 111",
+    "name": "SteelSeries Rival 300 Fallout 4 Vault Tec 111 Edition",
 
     "vendor_id": 0x1038,
     "product_id": 0x1712,
